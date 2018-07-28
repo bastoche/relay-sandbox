207ab1e7b3d03bfbc8f7eb6cd70ab9d3f326bb5e
Bastien Duret <bastien.duret@gmail.com>
2018-07-28 12:33:21 +0200

    First commit, adding eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "parserOptions": {
+         "ecmaVersion": 6,
+        "sourceType": "module",
+    },
+    "extends": "eslint:recommended", 
+    "plugins": ["prettier"],
+    "rules": {
+    "prettier/prettier": "error"
+  }
+};      
