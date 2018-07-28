617c015e239af6bd583de175b09257fc328da46f
Bastien Duret <bastien.duret@gmail.com>
2018-07-28 17:47:56 +0200

    Serve api with express-graphql

---
+++ src/server.js
@@ -0,0 +1,18 @@
+// @flow
+
+import express from "express";
+import graphqlHTTP from "express-graphql";
+
+import { schema } from "./schema";
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000);
