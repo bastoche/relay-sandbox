73ff2304531c810c95920317b87709afdf9f83c3
Bastien Duret <bastien.duret@gmail.com>
2018-07-29 13:05:27 +0200

    Improve typing

--- api/database.js
+++ api/database.js
@@ -3,22 +3,24 @@
 export class User {
   id: string;
   name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
 
 const VIEWER_ID = "me";
 
-const viewer = new User();
-viewer.id = VIEWER_ID;
-viewer.name = "Bobby";
+const viewer = new User(VIEWER_ID, "Bobby");
 
-const usersById = {
-  [VIEWER_ID]: viewer
-};
+export function getUser(id: string) {
+  if (id === VIEWER_ID) {
+    return viewer;
+  }
+  return null;
+}
 
 export function getViewer() {
   return getUser(VIEWER_ID);
-}
-
-export function getUser(id: string) {
-  return usersById[id];
 }
